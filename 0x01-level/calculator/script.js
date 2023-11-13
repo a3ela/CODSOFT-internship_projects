050451cedaf35e7ca0007f8f74535e638eb8aa9b
a3ela <abelsintayehu626@gmail.com>
2023-11-13 22:38:49 +0300

    functionality of the calculator

--- 0x01-level/calculator/script.js
+++ 0x01-level/calculator/script.js
@@ -1,38 +1,136 @@
 const initApp = () => {
-  const historyVal = document.getElementById("historyvalue");
-  const outputval = document.getElementById("outputvalue");
+  const currentValueEl = document.querySelector(".output-value");
+  const previousValEl = document.querySelector(".history-value");
   let itemArray = [];
-  let equationArray = [];
+  const equtionArray = [];
   let newNumberFlag = false;
 
-  const buttons = document.querySelectorAll(".number");
-  const clearBtn = document.getElementById("clear");
-  const deleteBtn = document.getElementById("backspace");
+  const inputButtons = document.querySelectorAll(".number");
+  const clearButton = document.querySelector(".clear");
+  const deleteButton = document.querySelector(".delete");
+  const signchangeButton = document.querySelector(".signChange");
+  const optButtons = document.querySelectorAll(".operator");
+  const equalsButton = document.querySelector(".equals");
 
-  buttons.forEach((button) => {
+  //numbers finctionality
+  inputButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
-      let currentval = e.target.textContent;
-
+      const newInput = e.target.textContent;
       if (newNumberFlag) {
-        outputval.innerText = currentval;
+        currentValueEl.value = newInput;
+        newNumberFlag = false;
       } else {
-        outputval.innerText =
-          outputval.innerText == 0
-            ? currentval
-            : `${outputval.innerHTML}${currentval}`;
+        currentValueEl.value =
+          currentValueEl.value == 0
+            ? newInput
+            : `${currentValueEl.value}${newInput}`;
       }
     });
   });
 
-  clearBtn.addEventListener("click", () => {
-    outputval.innerText = 0;
+  //clear functionality
+  clearButton.addEventListener("click", () => {
+    currentValueEl.value = 0;
+    previousValEl.textContent = "";
     itemArray = [];
   });
 
-  deleteBtn.addEventListener("click", () => {
-    if (outputval.innerText.length == 1) outputval.innerText = 0;
-    else outputval.innerText = outputval.innerText.slice(0, -1);
+  //equals functionality
+  equalsButton.addEventListener("click", () => {
+    const currentVal = currentValueEl.value;
+    let equationObj;
+
+    if (!itemArray.length && equtionArray.length) {
+      const lastEquation = equtionArray[equtionArray.length - 1];
+      equationObj = {
+        num1: parseFloat(currentVal),
+        num2: lastEquation.num2,
+        opt: lastEquation.opt,
+      };
+    } else if (!itemArray.length) {
+      return currentVal;
+    } else {
+      itemArray.push(currentVal);
+      equationObj = {
+        num1: parseFloat(itemArray[0]),
+        num2: parseFloat(currentVal),
+        opt: itemArray[1],
+      };
+    }
+    equtionArray.push(equationObj);
+
+    const equationString = `${equationObj["num1"]} ${equationObj["opt"]} ${equationObj["num2"]}`;
+    calculate(equationString, currentValueEl);
+    previousValEl.textContent = `${equationString} =`;
+
+    newNumberFlag = true;
+    itemArray = [];
+    console.log(equtionArray);
+  });
+
+  //delete functionality
+  deleteButton.addEventListener("click", () => {
+    if (currentValueEl.value.length == 1) currentValueEl.value = 0;
+    else currentValueEl.value = currentValueEl.value.slice(0, -1);
+  });
+
+  //signchanger functionality
+  signchangeButton.addEventListener("click", () => {
+    currentValueEl.value = parseFloat(currentValueEl.value) * -1;
+  });
+
+  //operators functionality
+  optButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      if (newNumberFlag) {
+        previousValEl.textContent = "";
+        itemArray = [];
+      }
+
+      const newOperator = e.target.textContent;
+      const currentVal = currentValueEl.value;
+
+      if (!itemArray.length && currentVal == 0) return;
+
+      //new equation
+      if (!itemArray.length) {
+        itemArray.push(currentVal, newOperator);
+        previousValEl.textContent = `${currentVal}${newOperator}`;
+        return (newNumberFlag = true);
+      }
+
+      //complete equation
+      if (itemArray.length) {
+        itemArray.push(currentVal);
+
+        const equationObj = {
+          num1: parseFloat(itemArray[0]),
+          num2: parseFloat(currentVal),
+          opt: itemArray[1],
+        };
+
+        equtionArray.push(equationObj);
+        const equationString = `${equationObj["num1"]}
+         ${equationObj["opt"]}
+         ${equationObj["num2"]}`;
+
+        const newValue = calculate(equationString, currentValueEl);
+
+        previousValEl.textContent = `${newValue} ${newOperator}`;
+
+        itemArray = [newValue, newOperator];
+        newNumberFlag = true;
+        console.log(equtionArray);
+      }
+    });
   });
 };
+document.addEventListener("DOMContentLoaded", initApp());
 
-document.addEventListener("DOMContentLoaded", initApp);
+const calculate = (equation, currentValEl) => {
+  const regex = /(^[*/=])|(\s)/g;
+  equation.replace(regex, "");
+  const divByZero = /(\/0)/.test(equation);
+  if (divByZero) return (currentValEl.value = "can't divide by zero");
+  return (currentValEl.value = eval(equation));
+};
